af22f96d66391496de64bda1dcd768409b41a75f
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2022-08-02 10:42:46 +0200

    Minor update

--- pages/quests.js
+++ pages/quests.js
@@ -319,7 +319,7 @@ export default function Home() {
           <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
               <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
           </svg>
-              <h2>You have completed the maximum number of quests for the moment. You will be able to do one more in {Math.floor(daysLeftBeforeQuest)} Days and {Math.floor((daysLeftBeforeQuest - Math.floor(daysLeftBeforeQuest)) * 24)} hours.</h2>
+              <h2>You have completed the maximum number of quests for the moment. You will be able to do one more in {Math.floor(daysLeftBeforeQuest)} days and {Math.floor((daysLeftBeforeQuest - Math.floor(daysLeftBeforeQuest)) * 24)} hours.</h2>
           </div> 
         </div>
       }
