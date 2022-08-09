f39942ab55ed515bae9db62ac0ab8e0b02ff158b
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2022-08-09 11:31:38 +0200

    Adding discord server quest

--- utils/questList.js
+++ utils/questList.js
@@ -61,7 +61,32 @@ export default [{
                                             <a href="https://starknet.id" target="_blank" rel="noreferrer"><button className="button gold">Open Starknet.id</button></a>
                                         ]
                                     }
-                                }
+                                },
+                                connected: [
+                                    {
+                                        name: "Server",
+                                        id: 10,
+                                        description: "Join our discord server ❤️",
+                                        long_description: "",
+                                        dependent: true,
+                                        icon: <svg className={styles.quest_point_icon} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+                                        </svg>,
+                                        steps: [6, 4],
+                                        customContent: [
+                                            <p>
+                                                Join our server discord, then click Okay
+                                            </p>,
+                                        ],
+                                        details: {
+                                            0: {
+                                                buttons: [
+                                                    <a href="https://discord.gg/E8yUd3SHqv" target="_blank" rel="noreferrer"><button className="button gold">Join</button></a>
+                                                ]
+                                            }
+                                        }
+                                    },
+                                ]
                             },
                             /*{
                                 devOnly: true,
@@ -89,7 +114,7 @@ export default [{
                                     {
                                         devOnly: true,
                                         name: "Tweet",
-                                        id: 8,
+                                        id: 9,
                                         description: "Make a tweet mentioning Eykar to support us ❤️",
                                         long_description: "Make a tweet containing at least @AgeOfEykar or eykar.org, with more than 50 characters.",
                                             icon: <svg className={styles.quest_point_icon} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
